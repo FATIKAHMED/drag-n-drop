2321e65beeab3114c326d6adcaddb26df88283d3
FATIKAHMED <mohammadhani110@gmail.com>
2023-05-18 20:02:35 +0500

    form not sortable

--- src/component/DragNDrop.js
+++ src/component/DragNDrop.js
@@ -1,32 +1,96 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
+import Form from "./Form";
+import update from "immutability-helper";
+import { useDrag } from "react-dnd";
+import { useDrop } from "react-dnd";
+import "./file.css";
+import { hover } from "@testing-library/user-event/dist/hover";
 
-
-const forms =(item)=>{
-return(
-    <input/>
-)
-}
-
-const iterate =()=>{
-
-}
+var FormList = [
+  {
+    id: 1,
+  },
+  {
+    id: 2,
+  },
+  {
+    id: 3,
+  },
+  {
+    id: 4,
+  },
+  {
+    id: 5,
+  },
+  {
+    id: 6,
+  },
+];
 
 function DragNDrop() {
-    const [items, setItems]=useState([])
-  return <div>
+  const [board, setBoard] = useState([]);
 
-    <div className="mainContainer">
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: "input",
+    drop: (item) => addFormToBoard(item.id),
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver(),
+    }),
+  }));
+
+  const addFormToBoard = (id) => {
+    FormList = FormList.filter((form) => id === form.id);
+    // setBoard((board) => [...board, FormList[0]]);
+    setBoard((board) => [...board, FormList[0]]);
+  };
+  // const moveCard = useCallback((dragIndex, hoverIndex) => {
+  //   setBoard((prevCards) =>
+  //     update(prevCards, {
+  //       $splice: [
+  //         [dragIndex, 1],
+  //         [hoverIndex, 0, prevCards[dragIndex]],
+  //       ],
+  //     })
+  //   );
+  // }, []);
+
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: "input",
+    item: { id: FormList.id },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
+
+  //trying to re arrange
+
+  const moveCard = useCallback((dragIndex, hoverIndex) => {
+    setBoard((board) =>
+      update(board, {
+        $splice: [
+          [dragIndex, 1],
+          [hoverIndex, 0, board[dragIndex]],
+        ],
+      })
+    );
+  }, []);
+
+  return (
+    <div>
+      <div className="mainContainer">
         <div className="firstGrid">
-        {item.map((item)=>{
-            <
-        })}
+          {FormList.map((form) => {
+            return <Form id={form.id} />;
+          })}
         </div>
-        <div className="secondGrid">
-
+        <div className="secondGrid" ref={drop}>
+          {board.map((form) => {
+            return <Form id={form.id} moveCard={moveCard} />;
+          })}
         </div>
-
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default DragNDrop;
--- src/component/Form.js
+++ src/component/Form.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { useDrag } from "react-dnd";
 
-function Form() {
+function Form({ id }) {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: "input",
+    item: { id: id },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
   return (
-    <div>
-      <input type="string" />
-    </div>
+    // <img
+    //   ref={drag}
+    //   src={url}
+    //   width="150px"
+    //   style={{ border: isDragging ? "5px solid pink" : "0px" }}
+    // />
+    <input ref={drag} id={id} />
   );
 }
 
