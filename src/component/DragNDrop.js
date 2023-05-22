0ca85943994bcf228ef3e34da1b95651618747b6
FATIKAHMED <mohammadhani110@gmail.com>
2023-05-22 14:23:01 +0500

    latest

--- src/component/DragNDrop.js
+++ src/component/DragNDrop.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import { useDrop } from "react-dnd";
-import { SortableContainer, SortableElement } from "react-sortable-hoc";
-import arrayMove from "array-move";
 import "./file.css";
 import Form from "./Form";
 
@@ -20,25 +18,6 @@ const FormList = [
   },
 ];
 
-const SortableForm = SortableElement(({ id, label }) => (
-  <Form id={id} key={id} label={label} />
-));
-
-const SortableFormList = SortableContainer(({ forms }) => {
-  return (
-    <div className="firstGrid">
-      {forms.map((form, index) => (
-        <SortableForm
-          key={form.id}
-          index={index}
-          id={form.id}
-          label={form.label}
-        />
-      ))}
-    </div>
-  );
-});
-
 function DragNDrop() {
   const [board, setBoard] = useState([]);
 
@@ -54,23 +33,17 @@ function DragNDrop() {
     const formList = FormList.filter((form) => id === form.id);
     setBoard((board) => [...board, formList[0]]);
   };
-
-  const onSortEnd = ({ oldIndex, newIndex }) => {
-    setBoard((board) => arrayMove(board, oldIndex, newIndex));
-  };
-
   return (
     <>
-      <SortableFormList forms={FormList} onSortEnd={onSortEnd} useDragHandle />
+      <div className="firstGrid">
+        {FormList.map((form) => {
+          return <Form id={form.id} key={form.id} />;
+        })}
+      </div>
       <div className="secondGrid" ref={drop}>
-        {board.map((form) => (
-          <SortableForm
-            key={form.id}
-            index={form.id}
-            id={form.id}
-            label={form.label}
-          />
-        ))}
+        {board.map((form) => {
+          return <Form id={form.id} key={form.id} label={form.label} />;
+        })}
       </div>
     </>
   );
